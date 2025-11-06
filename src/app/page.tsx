ef195f95a8e4916ba2bba1f9f367be80968f35c0
NuelGB <annuel.535240048@stu.untar.ac.id>
2025-11-06 14:20:51 +0700

    membuat menu unggulan di beranda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Card from "react-bootstrap/Card";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+    <div className={styles.container2}>
       <h1 className={styles.judulHalaman}>
         Nasi Goreng <span className={styles.kuning}>Pakde Har</span>
       </h1>
 
       <h3 className={styles.deskripsisingkatberanda}>
+        Bukan sekadar pengganjal lapar. Nikmati porsi brutal dengan bumbu khas yang otentik. 
+        Harga pas di kantong, rasa dijamin nendang!
+      </h3>
+      <a href="#menuUnggulan"><button className={styles.tombolJelajahi}>Ayo Jelajahi !</button></a>
+    </div>
+    <h2 className={styles.judulHalaman2} id="menuUnggulan">
+        Menu Unggulan Kami
+      </h2>
+
+      <h3 className={styles.deskripsisingkat2}>
         Bukan sekadar pengganjal lapar. Nikmati porsi brutal dengan bumbu khas yang otentik. 
         Harga pas di kantong, rasa dijamin nendang!
       </h3>
