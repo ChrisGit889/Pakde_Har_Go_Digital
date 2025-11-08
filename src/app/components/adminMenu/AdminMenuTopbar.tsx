18874361bfbdc72b49a45b3e7e32f5bb2d255e29
ChrisGit889 <christhiohermawan88@gmail.com>
2025-11-08 14:42:41 +0700

    Penambahan fitur menu 2

--- src/app/components/adminMenu/AdminMenuTopbar.tsx
+++ src/app/components/adminMenu/AdminMenuTopbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
-import Link from 'next/link'; 
-import './AdminMenu.css';
+import Link from 'next/link';
+import './AdminMenu.css'; 
 
 interface TopbarProps {
   onAddCategoryClick: () => void;
@@ -11,21 +11,36 @@ export default function AdminMenuTopbar({ onAddCategoryClick }: TopbarProps) {
   return (
     <div className="admin-menu-topbar">
       <h1 className="admin-menu-title">Daftar Produk</h1>
-      
-      <div className="search-wrapper">
-        <span>🔍</span>
-        <input type="text" placeholder="Cari Menu" />
-      </div>
-      <div className="button-group">
-        <button 
-          className="admin-button secondary" 
-          onClick={onAddCategoryClick} // <-- INI DIA
-        >
-          <span>+</span> Tambah Kategori
-        </button>
-        <Link href="/admin/menu/tambah" className="admin-button primary">
-          <span>+</span> Tambah Menu
-        </Link>
+      <div className="topbar-right-group">
+        <div className="search-wrapper">
+            <img 
+              src="/images/search.png"
+              alt="Cari"
+              className="topbar-icon" 
+            />
+            <input type="text" placeholder="Cari Menu" />
+        </div>
+        <div className="button-group">
+          <button 
+            className="admin-button secondary" 
+            onClick={onAddCategoryClick} 
+          >
+            <img 
+              src="/images/add.png"
+              alt="Tambah Kategori"
+              className="topbar-icon" 
+            />
+            Tambah Kategori
+          </button>
+          <Link href="/admin/menu/tambah" className="admin-button secondary"> 
+            <img 
+              src="/images/cutlery.png"
+              alt="Tambah Menu"
+              className="topbar-icon" 
+            />
+            Tambah Menu
+          </Link>
+        </div>
       </div>
     </div>
   );
